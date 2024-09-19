475b430b7bbc8e7f5ff2d110af882c5329d92635
JeJaMel <jjmelendezb15@gmail.com>
2024-09-18 22:33:19 -0400

    feat: Add OOP Exercise 1

---
+++ src/Section7/OOP1Challenge/OOP1/BankAccount.java
@@ -0,0 +1,109 @@
+package Section7.OOP1Challenge.OOP1;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class BankAccount {
+
+    private int account_number;
+    private double balance;
+    private String customerName;
+    private String email;
+    private String phone_number;
+
+    public int getAccount_number() {
+        return account_number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setAccount_number(int account_number) {
+        this.account_number = account_number;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public void deposit() {
+
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+        double amount = 0;
+        double accountBalance = getBalance();
+        System.out.println("\nYour currently balance is: " + accountBalance);
+
+        try {
+            System.out.println("How much do you want to deposit?");
+            amount = sc.nextDouble();
+            balance += amount;
+        } catch (InputMismatchException e) {
+            System.out.println("Please enter a valid number.");
+        }
+        System.out.println("Now your current balance is: " + this.balance + "\n");
+    }
+
+    public void withdraw() {
+
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+        double accountBalance = getBalance();
+        System.out.println("\nYour currently balance is: " + accountBalance);
+        double amount = 0.0;
+
+
+        try {
+            System.out.println("How much do you want to withdraw? (Max = " + accountBalance + ")");
+            amount = sc.nextDouble();
+            if (amount > accountBalance) {
+                System.out.println("Your current balance is: " + this.balance + " you can't withdraw.\n");
+                return;
+            }
+            balance -= amount;
+
+        } catch (InputMismatchException e) {
+            System.out.println("Please enter a valid number.");
+        }
+
+        System.out.println("Now your current balance is: " + this.balance + "\n");
+
+    }
+
+    public void printClientInfo() {
+        System.out.println("\n===================================================");
+        System.out.println("Account number: " + account_number);
+        System.out.println("Customer name: " + customerName);
+        System.out.println("Email: " + email);
+        System.out.println("Phone number: " + phone_number);
+        System.out.println("Balance: " + balance);
+        System.out.println("===================================================\n");
+
+    }
+}
---
+++ src/Section7/OOP1Challenge/OOP1/Main.java
@@ -0,0 +1,61 @@
+package Section7.OOP1Challenge.OOP1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BankAccount bankAccount = new BankAccount();
+        boolean action = true;
+        int choice = 0;
+
+        bankAccount.setAccount_number(23412);
+        bankAccount.setBalance(411);
+        bankAccount.setCustomerName("Jesus");
+        bankAccount.setEmail("JeJaMelDev@gmail.com");
+        bankAccount.setPhone_number("0414-2435491");
+
+        System.out.println(bankAccount.getAccount_number());
+
+        System.out.println("\nHi, " + bankAccount.getCustomerName() + ", what do you want to do? ");
+
+        while (action) {
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. See info");
+            System.out.println("4. Exit");
+
+            try {
+                Scanner scanner = new Scanner(System.in);
+                choice = scanner.nextInt();
+            } catch (InputMismatchException _) {
+
+            }
+
+            switch (choice) {
+
+                case 1:
+                    bankAccount.deposit();
+                    break;
+                case 2:
+                    bankAccount.withdraw();
+                    break;
+                case 3:
+                    bankAccount.printClientInfo();
+                    break;
+                case 4:
+                    action = false;
+                    break;
+                default:
+                    System.out.println("Please enter a valid number.");
+
+            }
+
+        }
+
+    }
+
+}
+
