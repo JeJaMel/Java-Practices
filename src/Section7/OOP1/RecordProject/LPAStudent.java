d0bf8ed54af694ead79e61881553a007a87ad335
JeJaMel <jjmelendezb15@gmail.com>
2024-09-21 22:21:56 -0400

    feat: Add record class

---
+++ src/Section7/OOP1/RecordProject/LPAStudent.java
@@ -0,0 +1,4 @@
+package Section7.OOP1.RecordProject;
+
+public record LPAStudent(String id, String name, String dateOfBirth, String classList) {
+}
--- src/Section7/OOP1/RecordProject/Main.java
+++ src/Section7/OOP1/RecordProject/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
     for (int i = 1; i<=5; i++){
-        Student S = new Student("S92300" + i,
+        LPAStudent S = new LPAStudent("S92300" + i,
                 switch(i){
             case 1 -> "Mary";
             case 2 -> "John";
@@ -16,6 +16,17 @@ public class Main {
         }, "05/11/1985", "Java Masterclass");
         System.out.println(S);
     }
+
+    Student pojoStudent = new Student("S923006", "Ann", "05/11/1985", "Java Masterclass");
+    LPAStudent recordStudent = new LPAStudent("S923007", "Bill", "05/11/1985", "Java Masterclass");
+
+    System.out.println(pojoStudent);
+    System.out.println(recordStudent);
+
+    pojoStudent.setClassList(pojoStudent.getClassList() + ", Java OCP Exam 829");
+
+    System.out.println(pojoStudent.getName() + " is taking " + pojoStudent.getClassList());
+        System.out.println(recordStudent.name() + " is taking " + recordStudent.classList());
 
 
 
