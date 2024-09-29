e3e223c30b1e90b0d27b7f3c473c06e4b49f7692
JeJaMel <jjmelendezb15@gmail.com>
2024-09-29 15:47:58 -0400

    feat: Add Polymorphism challenge1 part 2

--- src/Section8/OOP5/Polymorphism/Main.java
+++ src/Section8/OOP5/Polymorphism/Main.java
@@ -1,13 +1,29 @@
 package Section8.OOP5.Polymorphism;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-    Movie theMovie = new Adventure("Indiana Jones");
-    theMovie.watchMovie();
+        Scanner sc = new Scanner(System.in);
 
-
-
+        while(true) {
+            String input = """
+                    Enter type
+                    A = Adventure
+                    C = Comedy
+                    S = Science Fiction 
+                    """;
+            System.out.println(input);
+            String type = sc.nextLine();
+            if("Qq".contains(type)) {
+                break;
+            }
+            System.out.println("Enter the Movie title: ");
+            String movieTitle = sc.nextLine();
+            Movie theMovie = Movie.getMovie(type, movieTitle);
+            theMovie.watchMovie();
+        }
     }
 }
