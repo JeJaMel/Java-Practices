f58efce58d769c5b9189837a624cc2568313688b
JeJaMel <jjmelendezb15@gmail.com>
2024-10-22 00:12:40 -0400

    feat: Add arrays main

--- src/Section9/Arrays/Main.java
+++ src/Section9/Arrays/Main.java
@@ -1,5 +1,7 @@
 package Section9.Arrays;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -17,15 +19,49 @@ public class Main {
 
         int[] firstTen = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        for(int i = 0; i < myIntArray.length; i++) {
-            System.out.println("INT Array position "+i+ ": "+ myIntArray[i]);
-            System.out.println("DOUBLE Array position "+i+ ": "+ myDoubleArray[i]);
+        for (int i = 0; i < myIntArray.length; i++) {
+            System.out.println("INT Array position " + i + ": " + myIntArray[i]);
+            System.out.println("DOUBLE Array position " + i + ": " + myDoubleArray[i]);
 
         }
 
         int arraySize = firstTen.length;
-        System.out.println("\nFirst= "+ firstTen[0]);
-        System.out.println("Last= "+ firstTen[arraySize - 1]);
+        System.out.println("\nFirst= " + firstTen[0]);
+        System.out.println("Last= " + firstTen[arraySize - 1]);
+
+        int[] newArray;
+        newArray = new int[5];
+
+        for (int i = 0; i < newArray.length; i++){
+            newArray[i] = newArray.length - i;
+        }
+        for (int i = 0; i < newArray.length; i++){
+            System.out.print(newArray[i] + " ");
+        }
+        System.out.println();
+        //enhanced for
+        for (int j : newArray) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+
+        //if we want to manipulate, we can use the class java.util.Arrays
+        //it has a single property or field, named length
+        //it inherits java.util.Object's functionality
+
+        System.out.println(Arrays.toString(newArray));
+        System.out.println(Arrays.toString(firstTen));
+
+        Object objectVariable = newArray;
+        if(objectVariable instanceof int[]){
+            System.out.println("object variable is really an int array");
+        }
+
+        Object[] objectArray = new Object[3];
+        objectArray[0] = "Hello";
+        objectArray[1] = new StringBuilder("World");
+        objectArray[2] = newArray;
+
 
     }
 
