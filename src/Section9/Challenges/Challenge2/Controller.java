08b69c271a791108e7b6e4e4bfe73497b8bf8e56
JeJaMel <jjmelendezb15@gmail.com>
2024-10-23 09:28:36 -0400

    feat: Update Challenge2 readIntegers()

--- src/Section9/Challenges/Challenge2/Controller.java
+++ src/Section9/Challenges/Challenge2/Controller.java
@@ -1,10 +1,11 @@
 package Section9.Challenges.Challenge2;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Controller {
 
-    public int[] readIntegers(){
+    public int[] readIntegers() {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter ',' delimited integers: ");
@@ -12,12 +13,12 @@ public class Controller {
 
         int[] arr = new int[input.split(",").length];
 
-        for(int i = 0; i < input.split(",").length; i++){
+        for (int i = 0; i < input.split(",").length; i++) {
             arr[i] = Integer.parseInt(input.split(",")[i]);
         }
 
-
-
+        System.out.println("\nYour generated array is: ");
+        System.out.println(Arrays.toString(arr));
         return arr;
     }
 
