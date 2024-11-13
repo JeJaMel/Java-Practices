d02ced5ecd00b8d6bf876210b7bd6b1beaac2d2e
JeJaMel <jjmelendezb15@gmail.com>
2024-11-12 22:07:27 -0400

    feat: Update Classes

--- src/personal/UniversityProjects/DBComponent/DBComponent.java
+++ src/personal/UniversityProjects/DBComponent/DBComponent.java
@@ -2,23 +2,26 @@ package personal.UniversityProjects.DBComponent;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class DBComponent {
 
     private static final String CONFIG_FILE_PATH = "/personal/UniversityProjects/DBComponent/Config.properties";
+    private static final String QUERIES_FILE_PATH = "/personal/UniversityProjects/DBComponent/Queries.properties";
     private final Properties globalConfig = new Properties();
-    private final Properties connectionProperties = new Properties();
+    private final Properties queriesConfig = new Properties();
     private PoolManager poolManager;
 
     public DBComponent() throws IOException {
         loadConfig();
+        loadQueries();
     }
 
     public DBComponent(String dbName) throws IOException, SQLException, ClassNotFoundException {
         loadConfig();
+        loadQueries();
         initializeDatabase(dbName);
     }
 
@@ -28,6 +31,16 @@ public class DBComponent {
                 throw new IOException("Config.properties file not found.");
             }
             globalConfig.load(input);
+        }
+    }
+
+    private void loadQueries() throws IOException {
+        try (InputStream input = getClass().getResourceAsStream(QUERIES_FILE_PATH)) {
+            if (input == null) {
+                throw new IOException("Queries.properties file not found.");
+            }
+            queriesConfig.load(input);
+            System.out.println("Queries loaded: " + queriesConfig);
         }
     }
 
@@ -73,4 +86,101 @@ public class DBComponent {
             poolManager.returnConnection(connection);
         }
     }
+
+    public String getSentence(String schema, String queryName) {
+        String key = schema + "." + queryName;
+        if (!queriesConfig.containsKey(key)) {
+            System.out.println("Key " + key + " not found");
+            return null;
+        }
+        return queriesConfig.getProperty(key);
+    }
+
+    public String[] getParameters(String schema, String queryName) {
+        String sentence = getSentence(schema, queryName);
+        int paramCount = sentence.length() - sentence.replace("?", "").length();
+
+        if (paramCount == 0) {
+            return new String[0];
+        }
+
+        Scanner sc = new Scanner(System.in);
+        String[] params = new String[paramCount];
+
+        for (int i = 0; i < paramCount; i++) {
+            System.out.print("Parameter " + (i + 1) + ": ");
+            params[i] = sc.nextLine();
+        }
+
+        return params;
+    }
+
+
+    public ResultSet exe(String schema, String queryName) throws SQLException, InterruptedException, ClassNotFoundException {
+        String sentence = getSentence(schema, queryName);
+        if (sentence == null) {
+            throw new SQLException("Query not found in properties file: " + schema + "." + queryName);
+        }
+
+        Connection con = getConnection();
+        Statement stmt = con.createStatement();
+        stmt.executeQuery(sentence);
+        returnConnection(con);
+        return stmt.getResultSet();
+    }
+
+
+    public ResultSet exe(String schema, String queryName, String... params) throws SQLException, InterruptedException, ClassNotFoundException {
+        String sentence = getSentence(schema, queryName);
+        if (sentence == null) {
+            throw new SQLException("Query not found in properties file: " + schema + "." + queryName);
+        }
+
+        Connection con = getConnection();
+        PreparedStatement stmt = con.prepareStatement(sentence);
+
+        for (int i = 0; i < params.length; i++) {
+            stmt.setString(i + 1, params[i]);
+        }
+
+        ResultSet resultSet = null;
+
+        try {
+            if (sentence.trim().toUpperCase().startsWith("SELECT")) {
+                resultSet = stmt.executeQuery();
+            } else {
+                int affectedRows = stmt.executeUpdate();
+                System.out.println("Rows affected: " + affectedRows);
+            }
+        } finally {
+            returnConnection(con);
+        }
+
+        return resultSet;
+    }
+
+
+    public void printTable(ResultSet resultSet) throws SQLException {
+        ResultSetMetaData metaData = resultSet.getMetaData();
+        int columnCount = metaData.getColumnCount();
+
+        for (int i = 1; i <= columnCount; i++) {
+            System.out.printf("%-20s", metaData.getColumnName(i));
+        }
+        System.out.println();
+
+        for (int i = 1; i <= columnCount; i++) {
+            System.out.print("--------------------");
+        }
+        System.out.println();
+
+        while (resultSet.next()) {
+            for (int i = 1; i <= columnCount; i++) {
+                System.out.printf("%-20s", resultSet.getObject(i));
+            }
+            System.out.println();
+        }
+    }
+
+
 }
--- src/personal/UniversityProjects/DBComponent/Main.java
+++ src/personal/UniversityProjects/DBComponent/Main.java
@@ -1,24 +1,25 @@
 package personal.UniversityProjects.DBComponent;
 
 import java.io.IOException;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
+    public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException, InterruptedException {
 
         /*
         Here in the main, I will create a mini example app to try the DBComponent Class methods.
         -Option to change the db, from PSQL to MySQL, Different DB same main usage.
         -Option to test the Connection Pool
-        -Basic CRUD implementation
         */
+
         boolean run = true;
         int option;
         DBComponent db = new DBComponent();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hi, welcome to the  DBComponent test!");
+        System.out.println("Hi, welcome to the DBComponent test!");
         System.out.println("Please enter the name of the Db you want to connect...");
         String dbName = sc.nextLine();
         db.UseDatabase(dbName);
@@ -26,19 +27,54 @@ public class Main {
         while (run) {
             System.out.println(printMenu());
             option = sc.nextInt();
+            sc.nextLine();
 
             switch (option) {
-                case 1:
+                case 1: {
+                    System.out.println("Enter the schema:");
+                    String schema = sc.nextLine();
 
+                    System.out.println("Enter the query name:");
+                    String queryName = sc.nextLine();
+
+                    String[] params = db.getParameters(schema, queryName);
+
+                    ResultSet rs = db.exe(schema, queryName, params);
+
+                    if (rs != null) {
+                        db.printTable(rs);
+                    } else {
+                        System.out.println("Query executed successfully!");
+                    }
                     break;
+                }
 
-                case 2:
-
+                case 2: {
+                    System.out.println("Enter the name of the Db you want to connect...");
+                    String name = sc.nextLine();
+                    db.UseDatabase(name);
                     break;
+                }
 
-                case 3:
+                case 3: {
+                    System.out.println("Enter the schema:");
+                    String schema = sc.nextLine();
 
+                    System.out.println("Enter the query name:");
+                    String queryName = sc.nextLine();
+
+                    System.out.println("Enter the number of threads you want to test with:");
+                    int numThreads = sc.nextInt();
+                    sc.nextLine();
+
+                    String[] params = db.getParameters(schema, queryName);
+
+                    for (int i = 0; i < numThreads; i++) {
+                        MyThreads thread = new MyThreads(db, schema, queryName, params);
+                        thread.start();
+                    }
                     break;
+                }
 
                 case 4:
                     run = false;
@@ -48,14 +84,14 @@ public class Main {
                     System.out.println("Please enter a valid option!");
                     break;
             }
-
         }
+
 
     }
 
     public static String printMenu() {
         return """
-                    Please select an option below:
+                    \nPlease select an option below:
                 1. Execute Query (selected from Querys.properties)
                 2. Change Database
                 3. Test Connection Pool
